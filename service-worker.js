bcc5d56f83eafda94469c9084520003e22e21f5a
Andrew Zacharias <andyzack@gmail.com>
2020-01-25 06:53:50 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/andyzack.github.io/precache-manifest.353720ac95b6f6cd85e91cb62dbab5f0.js"
+  "/andyzack.github.io/precache-manifest.8fe730be61b7ef1573a19ff0ce8ec748.js"
 );
 
 self.addEventListener('message', (event) => {

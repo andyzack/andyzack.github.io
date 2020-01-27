1abfe5d76b0be2b8bb3522d2f6a3936d4b99beea
Andrew Zacharias <andyzack@gmail.com>
2020-01-27 07:18:25 +0000

    Resume update - add pdf and doc downloasd

--- src/modules/Resume/index.js
+++ src/modules/Resume/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import documentResume from '../../assets/docs/andrew-new.pdf';
+
+import iconPDF from '../../assets/images/download-pdf.svg';
+import iconDOC from '../../assets/images/download-doc.svg';
+import documentPDF from '../../assets/docs/andrew-zacharia.pdf';
+import documentDOC from '../../assets/docs/andrew-zacharia.doc';
 
 // eslint-disable-next-line no-unused-vars
 import { Page, Text, View, Document, StyleSheet, PDFViewer } from '@react-pdf/renderer';
@@ -9,11 +13,39 @@ const Resume = () => (
   <div className="w-100 dt no-repeat bg-center cover center w-100">
     <div className="mw8 center">
       <div className="pa2 pt0 tl">
+        <div class="tr ph2">
+          <a
+          className="f6 fw4 hover-white no-underline white-70 dib ml2 pv1 pl1 grow"
+          href={documentPDF}
+          target="_blank"
+          rel="noopener noreferrer"
+          >
+            <img
+              src={iconPDF}
+              className="aaz-icon-linkedin w2"
+              alt="Download resume in PDF format"
+            />
+          </a>
+
+          <a
+            className="f6 fw4 hover-white no-underline white-70 dib ml2 pv1 pl1 grow"
+            href={documentDOC}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={iconDOC}
+              className="aaz-icon-linkedin w2"
+              alt="Download resume in DOC format"
+            />
+          </a>
+        </div>
+
         {/* START POSTER */}
         <div className="w-100 ph2 relative overflow-hidden">
           <iframe
             title="Andrew's Resume"
-            src={documentResume}
+            src={documentPDF}
             height="800"
             allowtransparency={true.toString()}
             allowFullScreen={true}

799869916229849e69d779bd066051b4984bc7f9
Andrew Zacharias <andyzack@gmail.com>
2020-02-12 18:38:09 +1100

    skills component

--- src/modules/Footer/index.js
+++ src/modules/Footer/index.js
@@ -1,88 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import SKILLS from './skills'
 
-import logoHtml5 from '../../assets/images/logo-html5.png';
-import logoScsslogo from '../../assets/images/logo-scsslogo.png';
-import logoA11y from '../../assets/images/logo-a11y.png';
-import logoJavascriptlang from '../../assets/images/logo-javascriptlang.png';
-import logoReact from '../../assets/images/logo-react.png';
-import logoVue from '../../assets/images/logo-vue.png';
-import logoAngularjs from '../../assets/images/logo-angularjs.png';
-import logoPlay from '../../assets/images/logo-javaplay.png';
-import logoGitlogo from '../../assets/images/logo-gitlogo.png';
-import logoVscode from '../../assets/images/logo-vscode.png';
-import logoAdobe from '../../assets/images/logo-adobe.png';
-import logoInvision from '../../assets/images/logo-invision.png';
-import logoAbstract from '../../assets/images/logo-abstract.png';
+class Skill extends Component {
+  render() {
+    const { image, link } = this.props.skill;
+
+    return(
+      <a href={link}>
+        <img
+          src={image}
+          className="aaz-icon-html5 w2 bg-black-30 br-100 b--dark-gray ba pa1 ma1"
+          alt="HTML5"
+          target="_blank"
+          rel="noopener noreferrer"
+        />
+      </a>
+    )
+  }
+}
+
+class Skills extends Component {
+  render() {
+    return(
+      <div className="white">
+        {
+          SKILLS.map(SKILL => {
+            return(
+              <Skill key={SKILL.id} skill={SKILL} />
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
 
 const Footer = () => (
 <footer className="w-100 bg-black-60">
   <div className="mw8 w-100 center dt">
     <div className="tc db mt3">
-      <img
-        src={logoHtml5}
-        className="aaz-icon-html5 w2 bg-black-30 br-100 b--dark-gray ba pa1 ma1"
-        alt="HTML5"
-      />
-      <img
-        src={logoScsslogo}
-        className="aaz-icon-sass w2 bg-black-30 br-100 b--dark-gray ba pa1 ma1"
-        alt="SASS"
-      />
-      <img
-        src={logoA11y}
-        className="aaz-icon-a11y w2 bg-black-30 br-100 b--dark-gray ba pa1 ma1"
-        alt="A11y"
-      />
-      <img
-        src={logoJavascriptlang}
-        className="aaz-icon-javascript w2 bg-black-30 br-100 b--dark-gray ba pa1 ma1"
-        alt="Javascript"
-      />
-      <img
-        src={logoReact}
-        className="aaz-icon-react w2 bg-black-30 br-100 b--dark-gray ba pa1 ma1"
-        alt="React"
-      />
-      <img
-        src={logoVue}
-        className="aaz-icon-vue w2 bg-black-30 br-100 b--dark-gray ba pa1 ma1"
-        alt="Vue"
-      />
-      <img
-        src={logoAngularjs}
-        className="aaz-icon-angularjs w2 bg-black-30 br-100 b--dark-gray ba pa1 ma1"
-        alt="Angularjs"
-      />
-      <img
-        src={logoPlay}
-        className="aaz-icon-javaplay w2 bg-black-30 br-100 b--dark-gray ba pa1 ma1"
-        alt="JavaPlay"
-      />
-      <img
-        src={logoGitlogo}
-        className="aaz-icon-git w2 bg-black-30 br-100 b--dark-gray ba pa1 ma1"
-        alt="Git"
-      />
-      <img
-        src={logoVscode}
-        className="aaz-icon-vscode w2 bg-black-30 br-100 b--dark-gray ba pa1 ma1"
-        alt="VScode"
-      />
-      <img
-        src={logoAdobe}
-        className="aaz-icon-adobe w2 bg-black-30 br-100 b--dark-gray ba pa1 ma1"
-        alt="Adobe"
-      />
-      <img
-        src={logoInvision}
-        className="aaz-icon-invision w2 bg-black-30 br-100 b--dark-gray ba pa1 ma1"
-        alt="invision"
-      />
-      <img
-        src={logoAbstract}
-        className="aaz-icon-abstract w2 bg-black-30 br-100 b--dark-gray ba pa1 ma1"
-        alt="abstract"
-      />
+      <Skills />
     </div>
     <div className="white-50 tc v-mid pa2">
     © 2020 Andrew Arokiaraj Zacharia
---
+++ src/modules/Footer/skills.js
@@ -0,0 +1,96 @@
+import logoHtml5 from '../../assets/images/logo-html5.png';
+import logoScsslogo from '../../assets/images/logo-scsslogo.png';
+import logoA11y from '../../assets/images/logo-a11y.png';
+import logoJavascriptlang from '../../assets/images/logo-javascriptlang.png';
+import logoReact from '../../assets/images/logo-react.png';
+import logoVue from '../../assets/images/logo-vue.png';
+import logoAngularjs from '../../assets/images/logo-angularjs.png';
+import logoPlay from '../../assets/images/logo-javaplay.png';
+import logoGitlogo from '../../assets/images/logo-gitlogo.png';
+import logoVscode from '../../assets/images/logo-vscode.png';
+import logoAdobe from '../../assets/images/logo-adobe.png';
+import logoInvision from '../../assets/images/logo-invision.png';
+import logoAbstract from '../../assets/images/logo-abstract.png';
+
+const SKILLS = [
+  {
+    id: 1,
+    title: 'html',
+    link: 'https://www.html5rocks.com/en/',
+    image: logoHtml5
+  },
+  {
+    id: 2,
+    title: 'scss',
+    link: 'https://sass-lang.com/',
+    image: logoScsslogo
+  },
+  {
+    id: 3,
+    title: 'a11Y',
+    link: 'https://a11yproject.com/',
+    image: logoA11y
+  },
+  {
+    id: 4,
+    title: 'javascript',
+    link: 'https://javascript30.com/',
+    image: logoJavascriptlang
+  },
+  {
+    id: 5,
+    title: 'reactjs',
+    link: 'https://create-react-app.dev/',
+    image: logoReact
+  },
+  {
+    id: 6,
+    title: 'vuejs',
+    link: 'https://vuejs.org/v2/guide/',
+    image: logoVue
+  },
+  {
+    id: 7,
+    title: 'angularjs',
+    link: 'https://docs.angularjs.org/tutorial',
+    image: logoAngularjs
+  },
+  {
+    id: 8,
+    title: 'javaplay',
+    link: 'https://www.playframework.com/',
+    image: logoPlay
+  },
+  {
+    id: 9,
+    title: 'git',
+    link: 'https://git-scm.com/doc',
+    image: logoGitlogo
+  },
+  {
+    id: 10,
+    title: 'vscode',
+    link: 'https://code.visualstudio.com/docs',
+    image: logoVscode
+  },
+  {
+    id: 11,
+    title: 'adobe',
+    link: 'https://www.adobe.com/au/creativecloud.html',
+    image: logoAdobe
+  },
+  {
+    id: 12,
+    title: 'invision',
+    link: 'https://www.invisionapp.com/',
+    image: logoInvision
+  },
+  {
+    id: 13,
+    title: 'abstract',
+    link: 'https://www.abstract.com/',
+    image: logoAbstract
+  }
+]
+
+export default SKILLS;
